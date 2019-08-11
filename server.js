98a145f9fa42c33cce2160e8b2e8d9cc80b41b56
william-parker <edwinious@gmail.com>
2019-08-11 13:01:09 -0400

    add peopleArr and get people route - working on Insomnia

--- server.js
+++ server.js
@@ -5,4 +5,24 @@ const server = express();
 
 server.use(express.json());
 
+
+const peopleArr = [
+    { id: 1, name: "Luke" },
+    { id: 2, name: "Han" },
+    { id: 3, name: "Leia" },
+    { id: 4, name: "Darth" },
+    { id: 5, name: "Palpatine" },
+    { id: 6, name: "Indiana" }
+  ];
+  
+  let chores = [];
+  let choreId = 1;
+
+  // GET PEOPLE FROM DATABASE
+server.get("/people", (req, res) => {
+    peopleArr;
+    res.status(200).json(peopleArr);
+  });
+
+
 module.exports = server; 
