67c55aff6eea8d0f75a9101e3736ca74db058585
william-parker <edwinious@gmail.com>
2019-08-11 13:59:35 -0400

    CRUD operations done (delete WIP) and ANSWERS.md done

--- server.js
+++ server.js
@@ -25,4 +25,77 @@ server.get("/people", (req, res) => {
   });
 
 
+//  CRUD ENPOINTS FOR **CHORES**
+
+// POST CHORE
+server.post("/", (req, res) => {
+    const chore = req.body;
+  
+    chore.id = choreId;
+    choreId = choreId + 1;
+  
+    chores.push(chore);
+  
+    res.status(201).json(chores);
+  });
+
+  // GET CHORE FROM ID
+server.get("/:id", (req, res) => {
+    const chore = chores.find(chore => chore.id === Number(req.params.id));
+  
+    if (chore) {
+      res.status(200).json(chore);
+    } else {
+      res.status(404).json({ message: "Chore not found" });
+    }
+  });
+
+  // PUT (UPDATE) CHORE BY ID
+server.put("/:id", (req, res) => {
+    const id = parseInt(req.params.id, 10);
+  
+    let item = chores.findIndex(chore => chore.id === id);
+    if (!item) {
+      res(404).json({ message: "Chore not found" });
+    }
+    if (!req.body.notes) {
+      return res.status(400).send({
+        success: "false",
+        message: "notes is required"
+      });
+    } else if (!req.body.description) {
+      return res.status(400).send({
+        success: "false",
+        message: "description is required"
+      });
+    }
+  
+    const updatedChore = {
+      notes: req.body.notes,
+      description: req.body.description,
+      assignedTo: req.body.assignedTo
+    };
+  
+    chores.splice(item, 1, updatedChore);
+  
+    return res.status(201).send({
+      success: "true",
+      message: "Chore added successfully",
+      updatedChore
+    });
+  });
+
+// DELETE CHORE
+server.delete("/:id", (req, res) => {
+    chores = chores.filter(chore => chore.id !== Number(req.params.id));
+  
+    res.status(201).json(chores);
+  });
+
+
+  
+
+
+
+
 module.exports = server; 
---
+++ z - ANSWERS.md
@@ -0,0 +1,25 @@
+## Self-Study/Essay Questions
+
+Demonstrate your understanding of this Sprint's concepts by answering the following free-form questions. Edit this document to include your answers after each question. Make sure to leave a blank line above and below your answer so it is clear and easy to read by your Team Lead.
+
+- [ ] Mention two parts of Express that you learned about this week.
+
+## Express Router, Express Middleware.
+
+- [ ] What is Express Middleware?
+
+## Express Middleware is an array of functions that execute in the order it's presented to help handle our routes. 
+
+- [ ] How can a client send data to an API?
+
+## Via the "Four Horsemen of the "API"- calype  - (headers, body, params, query)
+
+
+- [ ] What can the API return to help clients know if a request was successful?
+
+## A code of "200" is the code for a successful request.
+
+- [ ] What Express feature can we use to partition an application into sub-applications?
+
+## By using Express Routing
+
